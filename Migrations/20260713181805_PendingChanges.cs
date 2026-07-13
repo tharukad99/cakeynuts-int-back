d1f52c1425f5e6b0b135e5b2d779c1aa3aedd4ae
Tharuka Dilshan <tharukad92@gmail.com>
2026-07-13 19:21:24 +0100

    a23

---
+++ Migrations/20260713181805_PendingChanges.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CakeyNuts.backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
